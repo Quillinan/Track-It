a61fa42bc0aeb81f6d86b2f79a06ea8ad26f6714
Milton Arthur Quillinan Machado Silva <mquillinan@corp.sendas>
2023-06-01 15:41:51 -0300

    basic infra

---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default function LoginPage() {
+  return <PageContainer></PageContainer>;
+}
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Roboto";
+  font-size: 24px;
+  text-align: center;
+`;
